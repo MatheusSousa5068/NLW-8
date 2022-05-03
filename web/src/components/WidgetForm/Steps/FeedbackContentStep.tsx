b76a085fa49fb079318dd3735492d279f59f963c
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-05-03 09:47:46 -0300

    return button

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import { FeedBackType, feedbackTypes } from '..';
-import CloseButton from '../../CloseButton';
+import React from "react";
+import { FeedBackType, feedbackTypes } from "..";
+import CloseButton from "../../CloseButton";
+import { ArrowLeft } from "phosphor-react";
 
 interface FeedBackContentStepProps {
-    feedbackType: FeedBackType
+    feedbackType: FeedBackType;
+    onFeedbackRestartRequested: () => void;
 }
 
-function FeedbackContentStep({ feedbackType}: FeedBackContentStepProps) {
-    const feedbackTypeInfo = feedbackTypes[feedbackType]
+function FeedbackContentStep({
+    feedbackType,
+    onFeedbackRestartRequested,
+}: FeedBackContentStepProps) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
         <>
             <header>
-                <span className="text-xl leading-6 flex justify-center gap-2"><img src={feedbackTypeInfo.image.source} className="w-6 h-6 justify-center" alt={feedbackTypeInfo.image.alt} /> {feedbackTypeInfo.title}</span>
+                <button
+                    type="button"
+                    onClick={onFeedbackRestartRequested}
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                >
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
+                </button>
+
+                <span className="text-xl leading-6 flex justify-center gap-2">
+                    <img
+                        src={feedbackTypeInfo.image.source}
+                        className="w-6 h-6 justify-center"
+                        alt={feedbackTypeInfo.image.alt}
+                    />{" "}
+                    {feedbackTypeInfo.title}
+                </span>
 
                 <CloseButton />
             </header>
-            <div className="flex py-8 gap-2 w-full">
-                
-            </div>
+            <div className="flex py-8 gap-2 w-full"></div>
         </>
     );
 }
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -36,17 +36,22 @@ export type FeedBackType = keyof typeof feedbackTypes;
 function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null);
 
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+
     const classStyles =
         "bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col text-center items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto";
 
     return (
         <div className={classStyles}>
-            
-
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedBackTypeChanged={setFeedbackType} />
             ) : (
-                <FeedbackContentStep feedbackType={feedbackType}/>
+                <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
 
             <footer className="text-xs text-neutral-400">
