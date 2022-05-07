92c93560710eb9065d6d0067531d6e1ae71fb330
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-05-06 21:37:18 -0300

    connecting node server to prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,25 @@
 import express from 'express'
+import { prisma } from './prisma'
 const app = express()
 
 const PORT = 3333
 
+app.use(express.json())
+
+
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    })
+
+    return res.status(201).json({ data: feedback })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
