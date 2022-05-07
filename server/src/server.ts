f15a3ed1963228dd0483570002cd36fbc98ddeee
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2022-05-06 21:49:52 -0300

    nodemailer working

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 const app = express()
+
+require('dotenv').config()
 
 const PORT = 3333
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASSWORD
+    }
+});
+
 
 
 app.post('/feedbacks', async (req, res) => {
@@ -16,6 +29,16 @@ app.post('/feedbacks', async (req, res) => {
             comment,
             screenshot
         }
+    })
+
+    await transport.sendMail({
+        from: "Equipe Feedget <team@feedget.com>",
+        to: 'Matheus Sousa <matheus5068sousa@gmail.com>',
+        subject: 'New feedback',
+        html: [
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+        ].join('\n')
     })
 
     return res.status(201).json({ data: feedback })
